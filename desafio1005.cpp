041a07901b1c72b09e537dd29830b568b982f3d1
Gustavo Bueno <gustavoriedel2202@gmail.com>
2022-07-14 22:33:38 -0300

    desafio 1005

---
+++ desafio1005.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+double a,b;
+double media;
+
+cout << fixed << setprecision(5);
+
+// cout<<"nota 1 ="<<endl;
+cin>> a;
+// cout<<"nota 2 ="<<endl;
+cin>> b;
+
+media = (a*3.5+b*7.5)/11;
+
+cout <<"MEDIA = " << media <<endl;
+
+return 0;
+}
